0e11288821e1531c7b0515132b77d5383009ab89
Kate Beard <asouthernbellein@gmail.com>
2018-09-11 17:27:00 +0100

    Relates #9 - add 500 server error test and codecov.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,7 +7,26 @@ const error = require('./error')
 router.use(bodyParser.urlencoded({extended: true}));
 router.get('/', home.get);
 
+// trigger 500 error for testing
+
+if(process.env.NODE_ENV === 'test') {
+  const triggerError = () => (req, res, next) => {
+  console.log('Testing 500')
+  try {
+    throw new Error('This function causes an error')
+  } catch (error) {
+    console.log("caught error");
+    next(error)
+  }
+};
+router.get("/error", triggerError());
+}
+
+// client error routes
+
 router.use(error.client);
 router.use(error.server);
+
+router.use(error.get);
 
 module.exports = router;
