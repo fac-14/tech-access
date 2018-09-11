034d0ad8f98b4ce1c29ec9f2102e4047f4b1c461
Kate Beard <asouthernbellein@gmail.com>
2018-09-11 16:13:49 +0100

    Relates #9 - build server, add basic home and error routes.

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const controllers = require('./controllers/index.js');
+// const helpers = require('./views/helpers/index.js');
+// const favicon = require('serve-favicon');
+
+const app = express();
+
+app.set('views', path.join (__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  handlebars({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+    //helpers,
+  })
+);
+
+app.set('port', process.env.PORT || 1337);
+app.set('host', process.env.PORT || 'localhost');
+//app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(controllers);
+
+module.exports = app;
