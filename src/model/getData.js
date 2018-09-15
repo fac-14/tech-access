715725c1c9641271b2309227024e4e516e2d6f6a
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-09-15 20:49:09 +0100

    implement getData query

--- src/model/getData.js
+++ src/model/getData.js
@@ -3,11 +3,11 @@ var inputArray = ["age"];
 const getResource = inputArray => {
   var dataQuery = `select resource.resource_id ,resource.resource_name,resource.url, demographic_tag. demographic_tag_id, demographic_tag.tag_name, demographic_resource_link.relevance from resource INNER JOIN demographic_resource_link ON resource.resource_id = demographic_resource_link.link_resource_id INNER JOIN demographic_tag ON demographic_resource_link.link_demographic_tag_id = demographic_tag. demographic_tag_id WHERE demographic_tag.tag_name = '${
     inputArray[0]
-  }'`;
+    }'`;
   // 'refugee' OR demographic_tag.tag_name = 'ethnicity' OR demographic_tag.tag_name = 'age' " ;
 
   //iterator appends users selected demographic tags
-  for (let i = 0; i < inputArray.length - 1; i++) {
+  for (let i = 1; i < inputArray.length; i++) {
     dataQuery += ` OR demographic_tag.tag_name = '${inputArray[i]}'`;
   }
   console.log("DATAQUERY=", dataQuery);
@@ -20,56 +20,54 @@ const getResource = inputArray => {
 };
 module.exports = getResource;
 
-console.log("HERE");
-
 // getResource(['age', 'disability', 'gender'])
 // .then(res => console.log(res.rows))
 // .catch(err => console.log(err))
+
+//function invoking/ function call
 const resultArr = getResource(["age", "disability", "gender"])
-  .then(res => {
-    var resultArr = res.rows;
-    var newAns = resultArr.reduce(reducer);
-    console.log("HELLO", newAns);
+  .then((res) => {
+    var inArray = res.rows;
+
+    var resultArray = []; //empty array to start with
+
+    for (let i = 0; i < inArray.length; i++) {
+      if (i == 0) {
+        //populate new resouce in our result array
+        //create new key demo_tag_relevance and push {tagname:'tagname value', relevance:'rel value'}
+        resultArrIndx = 0;
+        resultArray.push(res.rows[i]);
+        resultArray[resultArrIndx].demo_tag_relevance = [];
+        const b = {};
+        b.tag_name = inArray[i].tag_name;
+        b.relevance = inArray[i].relevance;
+        resultArray[resultArrIndx].demo_tag_relevance.push(b);
+        resultArrIndx++;
+
+
+      }
+      else if (resultArray[resultArrIndx - 1].resource_id == inArray[i].resource_id) {
+        resultArray[resultArrIndx - 1].demo_tag_relevance.push({ 'tag_name': inArray[i].tag_name, 'relevance': inArray[i].relevance });
+      }
+      else {
+        //populate new resouce in our result array
+        //create new key demo_tag_relevance and push {tagname:'tagname value', relevance:'rel value'}
+        resultArray.push(res.rows[i]);
+        resultArray[resultArrIndx].demo_tag_relevance = [];
+        const b = {};
+        b.tag_name = inArray[i].tag_name;
+        b.relevance = inArray[i].relevance;
+        resultArray[resultArrIndx].demo_tag_relevance.push(b);
+        resultArrIndx++;
+      }
+
+    } console.log('resultArray =', resultArray);
+    resultArray[2].demo_tag_relevance.forEach(function (officer) {
+      console.log(officer.tag_name, ":", officer.relevance);
+    })
   })
   .catch(err => console.log(err));
 
-////////
-
-//myArray.reduce(reducer)
-// var myarr = [1, 2, 3];
-// const reducer = (accumulator, currentvalue) => {
-//   return accumulator + currentvalue;
-// };
-
-// var sum = myarr.reduce(reducer);
-// console.log(sum);
-var accumulator = []; //empty array to start with
-const reducer = (accumulator, currentvalue) => {
-  // accumulator.forEach(function(element) {
-  //   if (element.resource_id == currentvalue.resource_id) {
-  //     //if one of the relevance is direct set our result's relevance to direct
-  //     if (element.relevance == "direct" || currentvalue.relevance == "direct") {
-  //       element.relevane = "direct";
-  //     }
-  //   } else {
-  //     //populate new resouce in our result array
-  //     accumulator.push(currentvalue);
-  //   }
-  // });
-  for (let i = 0; i < accumulator.length; i++) {
-    if (accumulator[i].resource_id == currentvalue.resource_id) {
-      if (
-        accumulator[i].relevance == "direct" ||
-        currentvalue.relevance == "direct"
-      ) {
-        accumulator[i].relevance = "direct";
-      }
-    } else {
-      //populate new resouce in our result array
-      accumulator.push(currentvalue);
-    }
-  }
-};
 
 //var newAns = resultArr.reduce(reducer);
 //console.log("HELLO", newAns);
