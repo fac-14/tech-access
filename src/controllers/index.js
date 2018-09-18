d3d9353db353803c0508916777147aae7e5f6815
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-09-18 16:39:02 +0100

    Update /results route to /sendinfo

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -7,29 +7,29 @@ const background = require('./background');
 const resources = require('./resources');
 const error = require('./error');
 const moreinfo = require('./moreinfo');
-const results = require('./results');
+const sendinfo = require('./sendinfo');
 
-router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 router.get('/', home.get);
 router.get('/welcome', welcome.get);
 router.get('/background', background.get);
 router.get('/resources', resources.get);
 router.get('/more-info', moreinfo.get);
-router.get('/results', results.post); //used to be results.get
+router.get('/sendinfo', sendinfo.get); //used to be results.get
 
 // trigger 500 error for testing
 
-if(process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'test') {
   const triggerError = () => (req, res, next) => {
-  console.log('Testing 500')
-  try {
-    throw new Error('This function causes an error')
-  } catch (error) {
-    console.log("caught error");
-    next(error)
-  }
-};
-router.get("/error", triggerError());
+    console.log('Testing 500')
+    try {
+      throw new Error('This function causes an error')
+    } catch (error) {
+      console.log("caught error");
+      next(error)
+    }
+  };
+  router.get("/error", triggerError());
 }
 
 // client error routes
