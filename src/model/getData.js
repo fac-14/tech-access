c3d60b45c956c6fd9bc5b400257ffb74615eac7d
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-09-18 16:15:33 +0100

    implement getData and data structure >relates #28 and #25

--- src/model/getData.js
+++ src/model/getData.js
@@ -1,15 +1,25 @@
 const db = require("../database/db_connection");
-var inputArray = ["age"];
-const getResource = inputArray => {
-  var dataQuery = `select resource.resource_id ,resource.resource_name,resource.url, demographic_tag. demographic_tag_id, demographic_tag.tag_name, demographic_resource_link.relevance from resource INNER JOIN demographic_resource_link ON resource.resource_id = demographic_resource_link.link_resource_id INNER JOIN demographic_tag ON demographic_resource_link.link_demographic_tag_id = demographic_tag. demographic_tag_id WHERE demographic_tag.tag_name = '${
-    inputArray[0]
-  }'`;
-  // 'refugee' OR demographic_tag.tag_name = 'ethnicity' OR demographic_tag.tag_name = 'age' " ;
+
+const getData = (bgArray, rscArray) => {
+  console.log('INPUTARR', bgArray, rscArray);
+  var dataQuery = `select resource.resource_id ,resource.resource_name,resource.url, resource.resource_type, demographic_tag. demographic_tag_id, demographic_tag.tag_name, demographic_resource_link.relevance from resource INNER JOIN demographic_resource_link ON resource.resource_id = demographic_resource_link.link_resource_id INNER JOIN demographic_tag ON demographic_resource_link.link_demographic_tag_id = demographic_tag. demographic_tag_id WHERE (demographic_tag.tag_name='${
+    bgArray[0]
+    }'`;
+
 
   //iterator appends users selected demographic tags
-  for (let i = 0; i < inputArray.length - 1; i++) {
-    dataQuery += ` OR demographic_tag.tag_name = '${inputArray[i]}'`;
+  for (let i = 1; i < bgArray.length; i++) {
+    dataQuery += ` OR demographic_tag.tag_name = '${bgArray[i].trim()}'`;
   }
+
+  dataQuery += `) AND (resource.resource_type = '${rscArray[0].trim()}'`;
+
+  for (let i = 1; i < rscArray.length; i++) {
+    dataQuery += ` OR resource.resource_type = '${rscArray[i].trim()}'`;
+  }
+
+  dataQuery += ')'
+
   console.log("DATAQUERY=", dataQuery);
 
   return new Promise((resolve, reject) => {
@@ -18,5 +28,5 @@ const getResource = inputArray => {
       .catch(err => reject(err));
   });
 };
-module.exports = getResource;
+module.exports = getData;
 
--- src/model/index.js
+++ src/model/index.js
@@ -1,9 +1,9 @@
 const getDemoTagId = require("./get_demo_tag_id");
 const getResourceDetail = require("./get_resource_detail");
-const getResource = require("./getData");
+const getData = require("./getData");
 
 module.exports = {
   getDemoTagId: getDemoTagId,
   getResourceDetail: getResourceDetail,
-  getResource: getResource
+  getData: getData
 };
