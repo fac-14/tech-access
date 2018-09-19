28664413d0d6e547fa23f719d572e6fe3d0cfd75
Kate Beard <asouthernbellein@gmail.com>
2018-09-19 10:57:28 +0100

    Test effect of host variable on heroku

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.engine(
 );
 
 app.set('port', process.env.PORT || 1337);
-app.set('host', process.env.PORT || 'localhost');
+// app.set('host', process.env.HOST || 'localhost');
 //app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
 app.use(sassMiddleware({
   src: __dirname,
