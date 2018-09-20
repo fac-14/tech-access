d7d8dad6812f04e2fc206def0995dd0d8e5bcef8
Kate Beard <asouthernbellein@gmail.com>
2018-09-20 11:10:12 +0100

    Relates #38 - more info page added.

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -3,7 +3,7 @@ exports.get = (req, res) => {
     layout: 'scrollable',
     progressamt: '100',
     title: 'Results',
-    pageInfo: 'Based on the checkboxes you have selected, here are your results.',
+    pageInfo: 'Based on the checkboxes you have selected, here are your results. This is a work in progress - if you feel there aren\'t many resources that apply to you, please come back again later. We\'ll be adding more as we go long.',
     results: req.cookies.results
   });
 }
